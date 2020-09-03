07c85686fe7eaa52a173113a9fad936c39bca1a4
thomaslantern <shikisha@hotmail.com>
2020-09-03 13:58:16 -0400

    begin adding inputs and tiles

--- Quest For the Great Machine/win32_platform.cpp
+++ Quest For the Great Machine/win32_platform.cpp
@@ -11,6 +11,7 @@ struct Render_State {
 
 Render_State render_state;
 
+#include "platform_inputs.cpp"
 #include "gfxrender.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -67,23 +68,90 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		680, 400, 0, 0, hInstance, 0);
 	HDC hdc = GetDC(window);
 
+	Input input = {};
 
 	int level = 1;
 
 	while (gameActive) 
 	{
+		
+
+		for (int i = 0; i < BUTTON_COUNT; i++) 
+		{
+			input.buttons[i].changed = false;
+		}
+		
 		//INPUT
 		MSG message;
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
 		{
-			TranslateMessage(&message);
-			DispatchMessage(&message);
+			switch (message.message)
+			{
+			case WM_KEYUP:
+			case WM_KEYDOWN:
+			{
+				unsigned int vk_code = (unsigned int)message.wParam;
+				bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+
+				switch (vk_code)
+				{
+				case VK_UP:
+				{
+					input.buttons[BUTTON_UP].is_down = is_down;
+					input.buttons[BUTTON_UP].changed = true;
+				} break;
+				case VK_DOWN:
+				{
+					input.buttons[BUTTON_DOWN].is_down = is_down;
+					input.buttons[BUTTON_DOWN].changed = true;
+				} break;
+				case VK_LEFT:
+				{
+					input.buttons[BUTTON_LEFT].is_down = is_down;
+					input.buttons[BUTTON_LEFT].changed = true;
+				} break;
+				case VK_RIGHT:
+				{
+					input.buttons[BUTTON_RIGHT].is_down = is_down;
+					input.buttons[BUTTON_RIGHT].changed = true;
+				} break;
+				}
+			} break;
+
+			default:
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			}
 		}
 		//SIMULATE
 		render_graphics(0XEEEEEE);
 		//draw_rect(70, 0, 70, 300, 0X000000);
-		//draw_rect(80, 0, 80, 40, 0X334422);
-		draw_rect(40, 10, 340, 350, 0X660000);
+		if (input.buttons[BUTTON_UP].is_down)
+			draw_rect(80, 0, 180, 140, 0X334422);
+		else if (input.buttons[BUTTON_DOWN].is_down)
+			draw_rect(180, 0, 280, 140, 0X552233);
+		else if (input.buttons[BUTTON_LEFT].is_down)
+			draw_rect(280, 0, 380, 100, 0x6633AA);
+		else if (input.buttons[BUTTON_RIGHT].is_down)
+			draw_rect(380, 0, 480, 100, 0x00FF00);
+
+		draw_rect(39, 99, 341, 301, 0X000000);
+		draw_rect(40, 100, 340, 300, 0X660000);
+		draw_rect(40, 100, 90, 150, 0XDDDDDD);
+		draw_rect(90, 100, 140, 150, 0XBBBBBB);
+		draw_rect(140, 100, 190, 150, 0XDDDDDD);
+		draw_rect(190, 100, 240, 150, 0XBBBBBB);
+
+
+		for (int row = 1; row < 7; row++) {
+			unsigned int* pixel = (unsigned int*)render_state.memory + x0 + row * render_state.width;
+			for (int col = 1; col < 7; col++) {
+				draw_rect()
+			}
+		}
+
+
 		
 		
 		
